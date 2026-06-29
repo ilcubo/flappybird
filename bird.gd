62c1d9b9a612f18f2c7ec378b4c11cee2478ad46
ilcubo <sasp3name16f@gmail.com>
2026-06-29 15:21:41 +0700

    ✨ Add basic jumping

---
+++ bird.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = JUMP_VELOCITY
+
+	move_and_slide()
